ca2a28a10e9da1243a8b735d2633ff3423c7de83
lasda1 <35976032+lasda1@users.noreply.github.com>
2023-03-27 19:10:55 +0200

    Update index.ts
    remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,16 @@
 import Express from "express";
 
-// ***************************************************
-// * Use global variables (see global.d.ts file for definitions)
-// ***************************************************
-
-global.Config = { Foo: "Bar" };
-global.Foo = "Bar";
-
-// ***************************************************
-// * Setup express
-// ***************************************************
 
 const app = Express();
 
-// ***************************************************
-// * Add a couple of routes
-// ***************************************************
-
 app.get('/ping', function (req: Express.Request, res: Express.Response) {
-	console.log(`${new Date().toISOString()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-	// req.UserID is available here because of the modification of the global namespace
 	res.json({ Pong: new Date() });
 });
 
-app.get('/attach-to-debugger', function (req: Express.Request, res: Express.Response) {
-
-	console.log("The debugger will auto attach here if run with for example `npm run tsc-watch`.");
-	debugger; // Use debugger statement or F9 to set breakpoints
-	console.log("Step to me by pressing F10");
-	res.json({ DebuggingCompleted: new Date() });
-});
-
-// ***************************************************
-// * Start listening on port 4000
-// ***************************************************
 
 app.listen(4000, () => {
 	console.log('Listening on port 4000. Try these URL:s');
 	console.log("http://localhost:4000/ping");
-	console.log("http://localhost:4000/attach-to-debugger");
 });
-
-// ***************************************************
-// * This is mandatory in all files
-// ***************************************************
 
 export { };
